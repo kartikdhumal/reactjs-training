acb45e695377b58e69a09719c465ff2341991daa
kartik <kartikdhumal24@gmail.com>
2025-03-18 17:37:35 +0530

    Skeleton in tenthProject

--- tenthProject/src/App.jsx
+++ tenthProject/src/App.jsx
@@ -1,61 +1,69 @@
-import React, { useState, useMemo, useRef, useCallback, forwardRef } from "react";
+import React, { useState, useMemo, useRef } from "react";
 import generateItems from "./utils/generateItems";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "./styles/home.scss";
+import { useEffect } from "react";
 
-const Item = forwardRef(({ item }, ref) => {
+const Item = ({ item, loadmore }) => {
+  const observerRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    if (!observerRef.current) return
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadmore()
+      }
+    });
+    if (observer) observer.observe(observerRef.current);
+    return () => observer.disconnect()
+  }, [observerRef, loadmore])
   return (
-    <div className="item" ref={ref}>
-      <img src={item.imageUrl} alt={item.name} loading="lazy" />
+    <div className="item" >
+      {loading && <Skeleton height={240} width={170} />}
+      <img ref={observerRef} src={item.imageUrl} alt={item.name} loading="lazy" onLoad={() => {
+        setLoading(false)
+      }} />
       <p>{item.name}</p>
     </div>
   );
-});
+};
 
 const items = generateItems(10000);
 
 const App = () => {
   const [search, setSearch] = useState("");
-  const [data] = useState(items);
-  const [visibleCount, setVisibleCount] = useState(20);
-  const [loading, setLoading] = useState(false);
-  const observerRef = useRef(null);
-
+  // const [visibleCount, setVisibleCount] = useState(4);
+  const [visibledata, setVisibledata] = useState([])
+  const [page, setPage] = useState(1)
   const filteredItems = useMemo(
-    () => data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase())),
-    [search, data]
+    () => items.filter((item) => item.name.toLowerCase().includes(search.toLowerCase())),
+    [search, items]
   );
+  useEffect(() => {
+    setVisibledata(items.slice(0, 4))
+  }, [filteredItems])
 
-  const lastItemRef = useCallback(
-    (node) => {
-      if (observerRef.current) observerRef.current.disconnect();
-      observerRef.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && !loading) {
-          setLoading(true);
-          setTimeout(() => {
-            setVisibleCount((prev) => prev + 20);
-            setLoading(false);
-          }, 3000);
-        }
-      });
-      if (node) observerRef.current.observe(node);
-    },
-    [loading]
-  );
+
+  const loadmore = () => {
+    setPage((prev) => {
+      const newPage = prev + 1
+      setVisibledata(filteredItems.slice(0, newPage * 4));
+      return newPage
+    })
+  }
+
 
   return (
     <div className="container">
-      <input
-        type="text"
-        className="search-bar"
-        placeholder="Search..."
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <input type="text" className="search-bar" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
       <div className="list">
-        {filteredItems.slice(0, visibleCount).map((item, index) => (
-          <Item key={item.id} item={item} ref={index === visibleCount - 1 ? lastItemRef : null} />
+        {visibledata.map((item, index) => (
+          <Item key={item.id} item={item} loadmore={index === visibledata.length - 1 ? loadmore : () => { }} />
         ))}
       </div>
-      {loading && <p className="loading-text">Loading more items...</p>}
     </div>
   );
 };
